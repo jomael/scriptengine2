6b7bf95c0b5ba991fdd84a760f5ddd6a18ef8863
eugene1 <eugene1@74ba6304-0836-4e1e-b292-7c7739985f1d>
2009-12-06 03:25:04 +0000

    Converted IO Package to Lazarus

--- trunk/IDE/packages/IO/units/uFileStreamModule.pas
+++ trunk/IDE/packages/IO/units/uFileStreamModule.pas
@@ -1,5 +1,9 @@
 unit uFileStreamModule;
 
+{$IFDEF FPC}
+{$MODE OBJFPC}{$H+}
+{$ENDIF}
+
 interface
 
 uses
@@ -71,7 +75,10 @@ end;
 
 function TFileStream_Create2(Self: TFileStream; AHandle: integer): TFileStream;
 begin
+  // units\uFileStreamModule.pas(74,39) Error: Wrong number of parameters specified for call to "Create"
+  {$IFNDEF FPC}
   result := TFileStream.Create(AHandle);
+  {$ENDIF}
 end;
 
 procedure CFileStreamModuleRegister(Module: TPackageModule; Data: Pointer; CallBack: TSE2PackageFunctionRegister);
