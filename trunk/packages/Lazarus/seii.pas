f6daa63eda6e75e90524a550531a8e7b3dc6e28c
eugene1 <eugene1@74ba6304-0836-4e1e-b292-7c7739985f1d>
2009-12-06 03:11:06 +0000

    Added Lazarus Package

---
+++ trunk/packages/Lazarus/seii.pas
@@ -0,0 +1,21 @@
+{ Diese Datei wurde automatisch von Lazarus erzeugt. Sie darf nicht bearbeitet 
+  werden!
+  Dieser Quelltext dient nur dem Übersetzen und Installieren des Packages.
+ }
+
+unit SEII; 
+
+interface
+
+uses
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('SEII', @Register); 
+end.
